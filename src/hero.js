fe302adf064e1c76e68e5cd700478e16ff749edd
Adridas <kirstukas.airidas@gmail.com>
2022-10-06 16:55:19 +0100

    Update hero.js

--- src/hero.js
+++ src/hero.js
@@ -6,7 +6,7 @@ function Hero() {
     return (
      <div className="hero" >
         <img src={mainimage} className="main-picture" alt="experiences"/>
-         <h1 className="hero-title">Online Experiences</h1> 
+         <h1 className="hero-title">Online experiences</h1> 
          <p className="hero-about">
          Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
 
